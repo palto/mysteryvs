7ba36738b6cefdddd741053a9fa264ca19b66b4c
Toni Paloniemi <toni.paloniemi@gmail.com>
2024-11-16 19:33:21 +0200

    Reset timer

--- components/ui/timer.tsx
+++ components/ui/timer.tsx
@@ -1,27 +1,57 @@
-import {useState} from "react";
-import {format} from "date-fns";
-import {useInterval} from "usehooks-ts";
-import {Button} from "@/components/ui/button";
+import { useState } from "react";
+import { format } from "date-fns";
+import { useInterval, useLocalStorage } from "usehooks-ts";
+import { Button } from "@/components/ui/button";
 
 export function Timer() {
-    const [startTime, setStartTime] = useState<number|undefined>(undefined);
-    const [timerText, setTimerText] = useState("");
+  const [timerText, setTimerText] = useState("");
 
-    useInterval(() => {
-        const running = !!startTime;
-        if(!running) {
-            return;
-        }
+  const [startTime, setStartTime] = useLocalStorage<number | undefined>(
+    "startTime",
+    undefined,
+    {
+      initializeWithValue: false,
+    },
+  );
 
-        const currentTime = Date.now();
-        const difference = currentTime - startTime;
+  const [completeTime, setCompleteTime, resetCompleteTime] = useLocalStorage<
+    number | undefined
+  >("completeTime", undefined, {
+    initializeWithValue: false,
+  });
 
-        setTimerText(format(difference, "mm:ss:SSS"));
-    }, 10);
+  const running = startTime && !completeTime;
 
-    return <>
-        <Button onClick={() => setStartTime(Date.now())}>Aloita</Button>
-        {timerText}
+  useInterval(() => {
+    if (!running) {
+      return;
+    }
+
+    const currentTime = Date.now();
+    const difference = currentTime - startTime;
+
+    setTimerText(format(difference, "mm:ss:SSS"));
+  }, 10);
+
+  function onComplete() {
+    setCompleteTime(Date.now());
+  }
+
+  return (
+    <>
+      {running && timerText}
+      {completeTime && format(completeTime - startTime!, "mm:ss:SSS")}
+      {running && <Button onClick={onComplete}>AIKA PÄÄTTYI!</Button>}
+      {!running && (
+        <Button
+          onClick={() => {
+            resetCompleteTime();
+            setStartTime(Date.now());
+          }}
+        >
+          AIKA ALKAA NYT!
+        </Button>
+      )}
     </>
-
+  );
 }
