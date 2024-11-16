a0b807b8a93acaba299bebc0d16b15fffacfd6cb
Toni Paloniemi <toni.paloniemi@gmail.com>
2024-11-16 16:39:20 +0200

    Timer doesn't start until start is pressed

--- components/ui/timer.tsx
+++ components/ui/timer.tsx
@@ -4,15 +4,20 @@ import {useInterval} from "usehooks-ts";
 import {Button} from "@/components/ui/button";
 
 export function Timer() {
-    const [startTime, setStartTime] = useState(Date.now());
+    const [startTime, setStartTime] = useState<number|undefined>(undefined);
     const [timerText, setTimerText] = useState("");
 
     useInterval(() => {
+        const running = !!startTime;
+        if(!running) {
+            return;
+        }
+
         const currentTime = Date.now();
         const difference = currentTime - startTime;
 
         setTimerText(format(difference, "mm:ss:SSS"));
-    }, 100);
+    }, 10);
 
     return <>
         <Button onClick={() => setStartTime(Date.now())}>Aloita</Button>
