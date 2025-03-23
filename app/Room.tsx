d0351f2f040a48d57c196d9d4b1fa7c4d9405697
Toni Paloniemi <toni.paloniemi@gmail.com>
2025-03-23 14:52:11 +0200

    Set default participants for store

--- app/Room.tsx
+++ app/Room.tsx
@@ -8,12 +8,28 @@ import {
 } from "@liveblocks/react/suspense";
 import { LiveList } from "@liveblocks/client";
 
+const defaultParticipants = [
+  "Anssi",
+  "Antti",
+  "Eetu",
+  "Jarkko",
+  "Jussi",
+  "Juuso",
+  "Jörö",
+  "Lauri",
+  "Pyry",
+  "Toni",
+];
+
 export function Room({ children }: { children: ReactNode }) {
   return (
     <LiveblocksProvider authEndpoint="/api/liveblocks-auth">
       <RoomProvider
         id="hevilan:lanit-2025-kevat"
-        initialStorage={{ name: "Lanit 2025", participants: new LiveList([]) }}
+        initialStorage={{
+          name: "Lanit 2025",
+          participants: new LiveList(defaultParticipants),
+        }}
       >
         <ClientSideSuspense fallback={<div>Loading…</div>}>
           {children}
