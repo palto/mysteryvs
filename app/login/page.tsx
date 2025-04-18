cf96982d8a7edb261f47e3eaace37b34e24d1e67
Toni Paloniemi <toni.paloniemi@gmail.com>
2025-04-18 13:06:24 +0300

    Make login page more cool

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,13 +11,15 @@ export default async function Login() {
 
   const participants = await getParticipants();
   return (
-    <div>
-      Valitse pelaajasi!
-      {participants.map((participant) => (
-        <div key={participant}>
-          <ParticipantLoginButton username={participant} />
-        </div>
-      ))}
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-bold mb-6">Valitse pelaajasi!</h1>
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
+        {participants.toSorted().map((participant) => (
+          <div key={participant}>
+            <ParticipantLoginButton username={participant} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
