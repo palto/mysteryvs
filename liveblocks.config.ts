49a326bc7d2fa10de85ceb1965e906bc6000f36d
Toni Paloniemi <toni.paloniemi@gmail.com>
2025-12-07 15:02:26 +0200

    Migrated to eslint 9

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -5,6 +5,7 @@ import { LiveList, LiveMap } from "@liveblocks/client";
 declare global {
   interface Liveblocks {
     // Each user's Presence, for useMyPresence, useOthers, etc.
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
     Presence: {
       // Example, real-time cursor coordinates
       // cursor: { x: number; y: number };
@@ -23,6 +24,7 @@ declare global {
     // Custom user info set when authenticating with a secret key
     UserMeta: {
       id: string;
+      // eslint-disable-next-line @typescript-eslint/no-empty-object-type
       info: {
         // Example properties, for useSelf, useUser, useOthers, etc.
         // name: string;
@@ -31,12 +33,14 @@ declare global {
     };
 
     // Custom events, for useBroadcastEvent, useEventListener
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
     RoomEvent: {};
     // Example has two events, using a union
     // | { type: "PLAY" }
     // | { type: "REACTION"; emoji: "🔥" };
 
     // Custom metadata set on threads, for useThreads, useCreateThread, etc.
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
     ThreadMetadata: {
       // Example, attaching coordinates to a thread
       // x: number;
@@ -44,6 +48,7 @@ declare global {
     };
 
     // Custom room info set with resolveRoomsInfo, for useRoomInfo
+    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
     RoomInfo: {
       // Example, rooms with a title and url
       // title: string;
