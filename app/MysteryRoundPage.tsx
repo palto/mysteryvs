80b032d56cab6f24dcd323a89c528c4ba45b60b1
Toni Paloniemi <toni.paloniemi@gmail.com>
2026-03-08 15:40:14 +0200

    Fix vertical scrollbar bug caused by topnav

--- app/MysteryRoundPage.tsx
+++ app/MysteryRoundPage.tsx
@@ -12,7 +12,7 @@ export function MysteryRoundPage() {
   const unsetHost = useUnsetHost();
   const startTime = useStartTime();
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-(family-name:--font-geist-sans)">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-(family-name:--font-geist-sans)">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div>
           Seuraa turnauksen edistymistä{" "}
--- app/login/LoginClientPage.tsx
+++ app/login/LoginClientPage.tsx
@@ -6,7 +6,7 @@ import { useParticipants } from "@/app/Participants";
 export function LoginClientPage() {
   const participants = useParticipants();
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center justify-center py-12">
       <h1 className="text-2xl font-bold mb-6">Valitse pelaajasi!</h1>
       <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4">
         {participants.toSorted().map((participant) => (
